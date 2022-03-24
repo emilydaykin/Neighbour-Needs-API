56f8e4f58dd06d0e3875ab6284d4fb90d9a95529
Mafalda-hub <78922064+Mafalda-hub@users.noreply.github.com>
2022-03-24 19:31:43 +0000

    corrected twice

--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const postSchema = new mongoose.Schema(
   {
-    tex: { type: String, required: true },
+    text: { type: String, required: true },
     category: { type: String, required: true },
     urgency: { type: String, required: true },
     createdBy: {
