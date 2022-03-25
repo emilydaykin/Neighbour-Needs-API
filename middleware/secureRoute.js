f7b6ca10c5f45e1ac865a6c65592568334504e4d
Emily Daykin <emilydaykin31@gmail.com>
2022-03-25 17:10:47 +0100

    fixed secureRoute bug

--- middleware/secureRoute.js
+++ middleware/secureRoute.js
@@ -2,22 +2,24 @@ import jwt from 'jsonwebtoken';
 import Profile from '../models/profile.js';
 import { secret } from '../config/environment.js';
 
-const secureRoute = async (res, req, next) => {
+// ! Remove explicit error message at deployement for security.
+const secureRoute = async (req, res, next) => {
   try {
-    console.log('req.headers.authorization', req.headers.authorization);
     const authToken = req.headers.authorization;
     if (!authToken || !authToken.startsWith('Bearer')) {
-      return res.status(401).send({ message: 'Unauthorised' });
+      return res
+        .status(401)
+        .send({ message: 'Unauthorised. Auth Token incorrect or does not exist' });
     } else {
       const token = authToken.replace('Bearer ', '');
       jwt.verify(token, secret, async (err, data) => {
         if (err) {
-          return res.status(400).json({ message: 'Unauthorised' });
+          return res.status(400).json({ message: "Unauthorised. JWT can't verify." });
         } else {
-          console.log('data', 'data');
-          const user = await Profile.findById(data.userId);
+          console.log('data', data);
+          const user = await Profile.findById(data.profileId);
           if (!user) {
-            return res.status(401).json({ message: 'Unauthorised' });
+            return res.status(401).json({ message: 'Unauthorised. User not in database' });
           } else {
             req.currentUser = user;
             next();
