ded8c4c18c283a8550b4e3478687c68886112a39
Mohamed Mohamed <m.h.mohamed@hotmail.co.uk>
2022-03-25 11:17:49 +0000

    User controller and changes to profile model

---
+++ controllers/userController.js
@@ -0,0 +1,56 @@
+import Profile from '../models/profile.js';
+import jwt from 'jsonwebtoken';
+import { secret } from '../config/environment.js';
+async function registerUser(req, res, next) {
+  try {
+    if (req.body.password !== req.body.passwordConfirmation) {
+      return res.status(422).json({ message: 'Passwords do not match' });
+    }
+
+    const profile = await Profile.create(req.body);
+    return res
+      .status(201)
+      .json({ message: 'Profile created !!!', body: profile });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function loginProfile(req, res, next) {
+  try {
+    const profile = await Profile.findOne({ email: req.body.email });
+
+    if (!profile) {
+      return res
+        .status(404)
+        .json({ message: 'Unauthorized, profile does not exist' });
+    }
+
+    const isValidPw = user.validatePassword(req.body.password);
+
+    if (!isValidPw) {
+      return res
+        .status(404)
+        .json({ message: 'Unauthorized, passwords do not match' });
+    }
+
+    const token = jwt.sign(
+      {
+        profileId: profile._id,
+        isAdmin: profile.isAdmin,
+        isHelper: profile.isHelper,
+      },
+      secret,
+      { expiresIn: '6h' }
+    );
+
+    return res.status(202).send({ token, message: 'Login successful!' });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export default {
+  registerUser,
+  loginUser,
+};
--- models/profile.js
+++ models/profile.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
-
+import uniqueValidator from 'mongoose-unique-validator';
+import { emailRegex, passwordRegex } from '../lib/stringTesters.js';
 const commentSchema = new mongoose.Schema(
   {
     text: { type: String, required: true, maxLength: 300 },
     rating: { type: Number, required: true, min: 1, max: 5 },
-    createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+    createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
   },
   { timestamps: true }
 );
@@ -12,8 +13,17 @@ const commentSchema = new mongoose.Schema(
 const profileSchema = new mongoose.Schema({
   firstName: { type: String, required: [true, 'First name required'] },
   surname: { type: String, required: [true, 'Surname required'] },
-  email: { type: String, required: [true, 'Email required'] },
-  password: { type: String, required: [true, 'Password required'] },
+  email: {
+    type: String,
+    required: [true, 'Email required'],
+    unique: true,
+    validate: (email) => emailRegex.test(email),
+  },
+  password: {
+    type: String,
+    required: [true, 'Password required'],
+    validate: (password) => passwordRegex.test(password),
+  },
   isHelper: { type: Boolean },
   averageRating: { type: String },
   services: { type: Array },
@@ -23,7 +33,9 @@ const profileSchema = new mongoose.Schema({
   imageService: { type: String },
   comments: [commentSchema],
   posts: { type: Array },
-  isAdmin: { type: Boolean }
+  isAdmin: { type: Boolean },
 });
+
+userSchema.plugin(uniqueValidator);
 
 export default mongoose.model('Profile', profileSchema);
