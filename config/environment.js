b59705d559af80300d1814ccd812e5ca646cc797
Emily Daykin <emilydaykin31@gmail.com>
2022-03-25 10:29:30 +0100

    added seed.js to connect, seed then disconnect from mongoDB

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,3 @@
 import dotenv from 'dotenv';
 
-export const dbURL =
-  process.env.dbURL || 'mongodb://127.0.0.1:27017/neighbourneeds';
+export const dbURL = process.env.dbURL || 'mongodb://127.0.0.1:27017/neighbourneeds';
