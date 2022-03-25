b67d64438f8c303899e97940d941b9d54b724de4
Emily Daykin <emilydaykin31@gmail.com>
2022-03-25 12:42:11 +0100

    added port and secret to environment config

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,8 @@
 import dotenv from 'dotenv';
+dotenv.config();
+
+export const port = process.env.PORT || 3002;
 
 export const dbURL = process.env.dbURL || 'mongodb://127.0.0.1:27017/neighbourneeds';
+
+export const secret = process.env.JWT_SECRET || 'sunAndBeach!';
