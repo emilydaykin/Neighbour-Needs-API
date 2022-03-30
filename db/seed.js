b0fcdb6172184fe5a31b61f348d3a9cbcbb740f4
Mohamed Mohamed <m.h.mohamed@hotmail.co.uk>
2022-03-30 15:45:45 +0100

    seeding comments complete

--- db/seed.js
+++ db/seed.js
@@ -8,10 +8,17 @@ const createPost = (text, service, urgency, createdBy) => {
     text: text,
     service: service,
     urgency: urgency,
-    createdBy: createdBy
+    createdBy: createdBy,
   };
 };
-
+function createComment(text, rating, createdBy) {
+  return {
+    text: text,
+    rating: rating,
+    createdBy: createdBy,
+  };
+}
+const jsonComment = JSON.stringify(createComment);
 async function seed() {
   await connectToDb();
   console.log('🤖 Connected to MongoDB!');
@@ -28,6 +35,7 @@ async function seed() {
   const randomUser1 = await Profile.findOne({ surname: 'Clending' });
   const randomUser2 = await Profile.findOne({ surname: 'Hoffmann' });
   const randomUser3 = await Profile.findOne({ surname: 'Long' });
+
   const generatedPosts = [
     createPost(
       "Hello there everyone! Can anyone help me plan my son's 6th birthday party next Saturday please?",
@@ -41,11 +49,45 @@ async function seed() {
       'cleaning, decorating',
       '',
       randomUser3._id
-    )
+    ),
   ];
+
+  const randomCommentee1 = profiles[0].comments;
+  console.log('randomCommentee1', randomCommentee1);
+  const randomCommentee2 = profiles[8].comments;
+  const randomCommentee3 = profiles[12].comments;
+  const randomCommentee4 = profiles[15].comments;
+
+  const generatedComments = [
+    randomCommentee1.push(
+      createComment(
+        'Absolutely amazing work feel truly honoured to have had you work with me!!!',
+        5,
+        randomUser1._id
+      )
+    ),
+    randomCommentee2.push(
+      createComment('Good work but turned up a little late', 3, randomUser2._id)
+    ),
+    randomCommentee3.push(
+      createComment(
+        'I would give a 1 but they turned up for the job so I thought that would be unfair',
+        2,
+        randomUser3._id
+      )
+    ),
+    randomCommentee4.push(
+      createComment(
+        'I would sincerly recommend any services for them, would give 5 but perfection is not attainable for humankind!!!',
+        4,
+        randomUser3._id
+      )
+    ),
+  ];
+  console.log(profiles[0], profiles[8]);
   const posts = await Post.create(generatedPosts);
   console.log(`🌱 Seeded ${posts.length} posts`);
-
+  console.log(`🌱 Seeded ${generatedComments.length} comments`);
   await disconnectFromDb();
   console.log('🔌 Disconnected from MongoDB!');
 }
