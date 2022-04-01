0bff050b7e8d78607690a9bf05cd24488e5daa0c
Emily Daykin <emilydaykin31@gmail.com>
2022-04-01 17:31:31 +0200

    deploying to heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { port } from './config/environment.js';
 import router from './config/router.js';
 import mongoSanitize from 'express-mongo-sanitize';
@@ -6,6 +7,7 @@ import { connectToDb } from './db/helpers.js';
 import errorHandler from './middleware/errorHandler.js';
 
 const app = express();
+app.use(cors()); // we're telling it: "this is an open api"
 app.use(express.json());
 app.use(mongoSanitize());
 app.use(
